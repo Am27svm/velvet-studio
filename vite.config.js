10aa71a9b31a3f68110017a42d58cf54678a0b6a
Atul Mall <atulmallji249@gmail.com>
2026-03-09 19:11:34 +0530

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: ["carola-untuneful-semisentimentally.ngrok-free.dev"],
-  },
-});
+})
